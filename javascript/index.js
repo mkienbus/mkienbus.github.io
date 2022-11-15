5d03b4f127b8d85fd821df44ff14516ed0f7b24a
Michael Kienbusch <mkienbus@gmail.com>
2022-11-15 12:26:45 -0700

    flatiron school cert permission issue, styling anchors

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,1 @@
-for( let i = 0; i < 100; i++)
-{
-    document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress as of 11/15/2022"));
-}
+document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress as of 11/15/2022"));
