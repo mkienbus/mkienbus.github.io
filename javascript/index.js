6fb7b94ba72512efc82007a1ba07782576a06081
Michael Kienbusch <mkienbus@gmail.com>
2022-11-15 12:01:16 -0700

    styling index and updated alert messge

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress"))
+document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress as of 11/15/2022"))
