1a9ceed4efcad8e0f1bd8b6547f313140415fd65
Michael Kienbusch <mkienbus@gmail.com>
2022-11-15 08:04:21 -0700

    messing with button

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,15 +1,21 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+
+//   }
+// )
+
 function handleClick() {
-    let dots = document.getElementById("dots");
-    let moreText = document.getElementById("more");
-    let btnText = document.getElementById("rezy-button");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("rezy-button");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
+}
