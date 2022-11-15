fb7f3b5b2c422e9e8e500bcfb907e1a9c4f932f0
Michael Kienbusch <mkienbus@gmail.com>
2022-11-15 11:05:46 -0700

    page is refreshing without event trigger happening

--- javascript/contactme.js
+++ javascript/contactme.js
@@ -1,1 +1,2 @@
-document.addEventListener("submit", document.location.reload(),10)
+// document.addEventListener("submit", document.location.reload(),10)
+document.addEventListener("submit", console.log("refreshed page"))
