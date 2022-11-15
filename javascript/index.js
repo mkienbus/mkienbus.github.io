c0dab89eb49cd158a570d42449aa50b1bdf7ee74
Michael Kienbusch <mkienbus@gmail.com>
2022-11-15 12:18:37 -0700

    syntax fixes

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,4 @@
-document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress as of 11/15/2022"))
-for( let i = 0; i < 1000000000; i++);
+for( let i = 0; i < 100; i++)
+{
+    document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress as of 11/15/2022"));
+}
