fe7469df8542b2071bdadaf321118852c20c1f5e
Michael Kienbusch <mkienbus@gmail.com>
2022-11-15 06:57:38 -0700

    test js button read more/less

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,7 +1,7 @@
 function handleClick() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("rezy-button");
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
