e51b9911cd671749eda49d3705d5c80c14dc2596
Michael Kienbusch <mkienbus@gmail.com>
2022-12-15 06:44:47 -0700

    update date

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress as of 12/5/2022, and is best viewed on desktop"));
+document.addEventListener("DOMContentLoaded", alert("This webpage is a current work in progress as of 12/15/2022, and is best viewed on desktop"));
