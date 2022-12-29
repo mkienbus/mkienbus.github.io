3d9687aa7dc84c3bede32e2c7e01e147ec460cb6
Michael Kienbusch <mkienbus@gmail.com>
2022-12-29 12:44:09 -0700

    back to formspree until i get mailto to work with all browsers

--- javascript/contactme.js
+++ javascript/contactme.js
@@ -1,7 +1,7 @@
-function onSubmit(){
-    let fname = document.getElementById("fname").value;
-    let lname = document.getElementById("lname").value;
-    let fullName = {fname} + { lname};
-    let message = document.getElementById("message").value;
-    window.location.replace(`mailto:mkienbus@gmail.com?subject=${fullName} contacted me through my website.&body="${message}"`);
-}
+// function onSubmit(){
+//     let fname = document.getElementById("fname").value;
+//     let lname = document.getElementById("lname").value;
+//     let fullName = {fname} + { lname};
+//     let message = document.getElementById("message").value;
+//     window.location.replace(`mailto:mkienbus@gmail.com?subject=${fullName} contacted me through my website.&body="${message}"`);
+// }
