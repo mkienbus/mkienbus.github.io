77e2e90a42c67c5e1972999e3da4ed987234ae9d
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-02-15 13:26:15 -0700

    added reset form button

--- javascript/contactme.js
+++ javascript/contactme.js
@@ -1,7 +1,3 @@
-// function onSubmit(){
-//     let fname = document.getElementById("fname").value;
-//     let lname = document.getElementById("lname").value;
-//     let fullName = {fname} + { lname};
-//     let message = document.getElementById("message").value;
-//     window.location.replace(`mailto:mkienbus@gmail.com?subject=${fullName} contacted me through my website.&body="${message}"`);
-// }
+function resetForm() {
+    document.getElementById("contactform").reset();
+}
